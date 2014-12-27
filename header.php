5ef61a323ee16528490c8df7f456dcddbd9844f3
Leon Denise <lowpolygonum@gmail.com>
2014-12-27 19:40:10 +0100

    Hop

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <?php add_action( 'wp_head', 'mp6_override_toolbar_margin', 11 );
+function mp6_override_toolbar_margin() {
+	if ( is_admin_bar_showing() ) { ?>
+		<style type="text/css" media="screen">
+			html { margin-top: 0px !important; }
+			* html body { margin-top: 0px !important; }
+		</style>
+	 <?php }
+} ?>
+<head profile="http://gmpg.org/xfn/11">
+ 
+	<title>DE-HORS</title>
+ 
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- leave this for stats -->
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	
+     <script src="//use.typekit.net/qpc7vmt.js"></script>
+	 <script>try{Typekit.load();}catch(e){}</script>
+ 	<?php wp_head(); ?>
+     
+
+
+</head>
+<body>
