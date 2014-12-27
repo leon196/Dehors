ac8ac946df680cdc749512293913011dc175f416
Simon Denise <simon@imacsimon-pc.home>
2014-12-27 19:38:03 +0100

    ok

--- index.php
+++ index.php
@@ -1,17 +1,92 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<?php include("header.php"); ?>
+      <!-- BACKGROUND -->
+        <div class="background noselect">
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+            <!-- BARRE -->
+            <div class="barre"></div>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+            <!-- COLUMN 1 -->
+            <div class="column" id="column1">
+
+                <!-- ROWS -->
+                <div class="row1">
+                    <div class="bigLetter">D</div>
+                </div>
+                <div class="row2">
+                    <div class="bigLetter">H</div>
+                </div>
+                <div class="row3">
+                    <div class="smallLetter select">
+LILIANA MOTTA, artiste botaniste<br/>
+avec la collaboration de<br/>
+YVES-MARIE ALLAIN, ingénieur horticole <br/>
+SÉBASTIEN ARGANT, paysagiste<br/>
+DAVID BELAMY, paysagiste <br/>
+JEAN-MARIE BOURGES, paysagiste
+                    </div>
+                </div>
+            </div>
+
+            <!-- COLUMN 2 -->
+            <div class="column" id="column2">
+
+                <!-- ROWS -->
+                <div class="row1">
+                    <div class="bigLetter">E</div>
+                </div>
+                <div class="row2">
+                    <div class="bigLetter">O</div>
+                </div>
+                <div class="row3">
+                    <div class="smallLetter select">
+ADRIEN BIEWERS, paysagiste <br/>
+YVAN CAPPELAERE, paysagiste <br/>
+JEAN-CHRISTOPHE DENISE, architecte <br/>
+LÉON DENISE, game developer <br/>
+SIMON DENISE, graphiste <br/>
+PATRICK DEGEORGES, philosophe 
+                    </div>
+                </div>
+            </div>
+
+            <!-- COLUMN 3 -->
+            <div class="column" id="column3">
+
+                <!-- ROWS -->
+                <div class="row2">
+                    <div class="bigLetter">R</div>
+                </div>
+                <div class="row3">
+                    <div class="smallLetter select">
+CAPUCINE DUFOUR, paysagiste <br/>
+CAMILLE FRECHOU, paysagiste<br/>
+THOMAS IRASQUE, paysagiste <br/>
+ALEXANDERE MALFAIT, paysagiste<br/>
+LAURENCE ROBERT, artiste paysagiste<br/>
+DAMIEN ROGER, paysagiste 
+                    </div>
+                </div>
+            </div>
+
+            <!-- COLUMN 4 -->
+            <div class="column" id="column4">
+
+                <!-- ROWS -->
+                <div class="row2">
+                    <div class="bigLetter">S</div>
+                </div>
+                <div class="row3">
+                    <div class="smallLetter select">
+LAURA ROUBINET, paysagiste<br/>
+ADRIEN ROUSSEAU, artiste paysagiste<br/>
+LINDA SEYVE, artiste paysagiste<br/>
+LUC SCHUHMACHER, artiste<br/>
+MARGAUX VIGNE, paysagiste<br/>
+CYRILLE WEINER, photographe
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+</body>
+</html>
